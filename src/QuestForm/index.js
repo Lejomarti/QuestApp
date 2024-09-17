0c2e251f1579160bdb4c54d45e4056e1f15c5886
AlejoMarti <alejomarti95@gmail.com>
2024-09-17 15:21:38 -0500

    imagen añadida

--- src/QuestForm/index.js
+++ src/QuestForm/index.js
@@ -2,43 +2,49 @@ import React from "react";
 import "./QuestForm.css";
 import { CreateTodoButton } from "../CreateTodoButton/CreateTodoButton";
 import { TodoContext } from "../TodoContext";
+import blankieStickers from "../App/blankie_stickers.png";
 
 function QuestForm() {
-
-  const {
-    addQuest
-  } = React.useContext(TodoContext)
-  const [newQuestTextAreaValue, setNewQuestTextAreaValue ] = React.useState('')
+  const { addQuest } = React.useContext(TodoContext);
+  const [newQuestTextAreaValue, setNewQuestTextAreaValue] = React.useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
     addQuest(newQuestTextAreaValue);
   };
 
-  const onErase = (event)=>{
+  const onErase = (event) => {
     event.preventDefault();
-    const formBox = event.target.closest(".form-box")
-    const textArea = formBox.querySelector("textarea")
-    textArea.value = ""
-  }
+    const formBox = event.target.closest(".form-box");
+    const textArea = formBox.querySelector("textarea");
+    textArea.value = "";
+  };
 
-  const onChange = (event)=>{
-    setNewQuestTextAreaValue(event.target.value)
-  }
+  const onChange = (event) => {
+    setNewQuestTextAreaValue(event.target.value);
+  };
 
   return (
-    <form className="form-box" onSubmit={onSubmit}>
-      <div className="form-box-content">
-        <h1 className="form-create-quest-text title">Create a new quest</h1>
-        <textarea id="quest-textArea" placeholder="Write a new quest" value={newQuestTextAreaValue} onChange={onChange}/>
-        <div className="buttons-container">
-          <CreateTodoButton type="submit" />
-          <button type="button" className="erase-button" onClick={onErase}>
-            Erase
-          </button>
+    <div className="box-container">
+      <img id="blankieStickers" src={blankieStickers} />
+      <form className="form-box" onSubmit={onSubmit}>
+        <div className="form-box-content">
+          <h1 className="form-create-quest-text title">Create a new quest</h1>
+          <textarea
+            id="quest-textArea"
+            placeholder="Write a new quest"
+            value={newQuestTextAreaValue}
+            onChange={onChange}
+          />
+          <div className="buttons-container">
+            <CreateTodoButton type="submit" />
+            <button type="button" className="erase-button" onClick={onErase}>
+              Erase
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
